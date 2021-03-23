6e65896191c3d75373ce3933ca46d929e2a7298d
Jonathon Albert <Jonathon.albert@realifetech.com>
2021-03-23 18:49:10 +0000

    Implemented basic Login and Register views

---
+++ LanguageApp/Login/Login/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  LanguageApp
+//
+//  Created by Jonathon Albert on 23/03/2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func logInButtonTapped(_ sender: Any) {
+        guard emailTextField.text != "", passwordTextField.text != "" else { return }
+        // TODO: Login
+    }
+}
---
+++ LanguageApp/Login/Register/RegisterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterViewController.swift
+//  LanguageApp
+//
+//  Created by Jonathon Albert on 23/03/2021.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
